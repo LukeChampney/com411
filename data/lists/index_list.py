10730aae0b6b3f7b905a7c65f0473804a2d12173
repl.it user <replituser@example.com>
2020-10-27 09:26:41 +0000

    Messed up in index_list, now fixed

--- data/lists/index_list.py
+++ data/lists/index_list.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-#define a function
-def run():
-
-  #define an another function
-  def movements():
-    #adding item/number in the list
-    path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
-    return path
-
-  #define an another function
-  def run():
-    print("Moving...")
-    path = movements()
-    #chooses to print whatever in the list
-=======
 def run():
 
   def movements():
@@ -23,14 +7,11 @@ def run():
   def run():
     print("Moving...")
     path = movements()
->>>>>>> 8f3df700ba3ca0603299935ec14f13f2c2f507d1
+
     print(f"{path[0]} for {path[1]} steps")
     print(f"{path[2]} for {path[3]} steps")
     print(f"{path[4]} for {path[5]} steps")
     print(f"{path[6]} for {path[7]} steps")
 
-<<<<<<< HEAD
   #calls the function
-=======
->>>>>>> 8f3df700ba3ca0603299935ec14f13f2c2f507d1
   run()
